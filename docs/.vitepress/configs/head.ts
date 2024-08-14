4c8abf54940e1c11329f98a84eb19973b95fc0dc
sunxuecong <1668840900@qq.com>
2024-08-14 18:10:25 +0800

    feat: 修改 head

--- docs/.vitepress/configs/head.ts
+++ docs/.vitepress/configs/head.ts
@@ -1,8 +1,7 @@
 import type { HeadConfig } from 'vitepress'
 
 const isDevelopment = process.env.NODE_ENV === 'development'
-const preUrl = '/github-actions-demo/'
-// const preUrl = process.env.APP_BASE_PATH || '/' 
+const preUrl = process.env.APP_BASE_PATH || '/' 
 
 
 export const head: HeadConfig[] = [
@@ -12,12 +11,7 @@ export const head: HeadConfig[] = [
   ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
   ['meta', { name: 'msapplication-TileColor', content: '#000000' }],
   ['meta', { name: 'msapplication-TileImage', content: preUrl + 'favicon.svg' }],
-  ['meta', { name: 'baidu-site-verification', content: 'codeva-Whjnr38WFE' }],
   ['link', { rel: 'apple-touch-icon', href:preUrl +  'favicon.svg' }],
   // ['link', { rel: 'mask-icon',type: 'image/svg+xml', href: preUrl + 'favicon.svg', color: '#3eaf7c' }],
   ['link', { rel: 'manifest', href: preUrl +'manifest.webmanifest' }],
-  [
-    'script',
-    { src: isDevelopment ? '' : 'https://hm.baidu.com/hm.js?8092fab2f2adfc7938ba5b8885aef5b4' }
-  ]
 ]
