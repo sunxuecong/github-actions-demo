826accfb49550135a21971d0631b68c1b7cde7ef
sunxuecong <1668840900@qq.com>
2024-08-14 17:52:20 +0800

    faet: 移除背景色

--- docs/.vitepress/configs/head.ts
+++ docs/.vitepress/configs/head.ts
@@ -7,14 +7,14 @@ const preUrl = '/github-actions-demo/'
 
 export const head: HeadConfig[] = [
   ['link', { rel: 'icon', type: 'image/svg+xml', href:preUrl +  'favicon.svg' }],
-  ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+  // ['meta', { name: 'theme-color', content: '#3eaf7c' }],
   ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
   ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
   ['meta', { name: 'msapplication-TileColor', content: '#000000' }],
   ['meta', { name: 'msapplication-TileImage', content: preUrl + 'favicon.svg' }],
   ['meta', { name: 'baidu-site-verification', content: 'codeva-Whjnr38WFE' }],
   ['link', { rel: 'apple-touch-icon', href:preUrl +  'favicon.svg' }],
-  ['link', { rel: 'mask-icon',type: 'image/svg+xml', href: preUrl + 'favicon.svg', color: '#3eaf7c' }],
+  // ['link', { rel: 'mask-icon',type: 'image/svg+xml', href: preUrl + 'favicon.svg', color: '#3eaf7c' }],
   ['link', { rel: 'manifest', href: preUrl +'manifest.webmanifest' }],
   [
     'script',
