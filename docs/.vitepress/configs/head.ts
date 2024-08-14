363922c547bcc30a8a96d1a341e9c92d814f9181
sunxuecong <1668840900@qq.com>
2024-08-14 17:46:34 +0800

    feat: 这次一定能行

--- docs/.vitepress/configs/head.ts
+++ docs/.vitepress/configs/head.ts
@@ -6,16 +6,16 @@ const preUrl = '/github-actions-demo/'
 
 
 export const head: HeadConfig[] = [
-  ['link', { rel: 'icon', type: 'image/svg+xml', href:preUrl +  '/favicon.svg' }],
+  ['link', { rel: 'icon', type: 'image/svg+xml', href:preUrl +  'favicon.svg' }],
   ['meta', { name: 'theme-color', content: '#3eaf7c' }],
   ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
   ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
   ['meta', { name: 'msapplication-TileColor', content: '#000000' }],
-  ['meta', { name: 'msapplication-TileImage', content: preUrl + '/favicon.svg' }],
+  ['meta', { name: 'msapplication-TileImage', content: preUrl + 'favicon.svg' }],
   ['meta', { name: 'baidu-site-verification', content: 'codeva-Whjnr38WFE' }],
-  ['link', { rel: 'apple-touch-icon', href:preUrl +  '/favicon.svg' }],
-  ['link', { rel: 'mask-icon',type: 'image/svg+xml', href: preUrl + '/favicon.svg', color: '#3eaf7c' }],
-  ['link', { rel: 'manifest', href: preUrl +'/manifest.webmanifest' }],
+  ['link', { rel: 'apple-touch-icon', href:preUrl +  'favicon.svg' }],
+  ['link', { rel: 'mask-icon',type: 'image/svg+xml', href: preUrl + 'favicon.svg', color: '#3eaf7c' }],
+  ['link', { rel: 'manifest', href: preUrl +'manifest.webmanifest' }],
   [
     'script',
     { src: isDevelopment ? '' : 'https://hm.baidu.com/hm.js?8092fab2f2adfc7938ba5b8885aef5b4' }
