ace44003524a62030bd0a62e459427b20020724d
sunxuecong <1668840900@qq.com>
2024-11-29 15:04:17 +0800

    feat: 修改部分代码

--- docs/.vitepress/configs/sidebar.ts
+++ docs/.vitepress/configs/sidebar.ts
@@ -2,6 +2,7 @@ import type { DefaultTheme } from "vitepress";
 
 export const sidebar: DefaultTheme.Config["sidebar"] = {
   "/front/": [
+    
     // {
     //   text: "JavaScript 基础知识",
     //   collapsed: false,
